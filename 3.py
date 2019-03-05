a57f36bf669194d96a092c7f41231bea717c53b3
NievesLaura <48244869+NievesLaura@users.noreply.github.com>
2019-03-05 12:24:40 +0100

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/bash   pyhton 3.py
+# __*__   coding:utf-8  __*__
+def main():
+    print("ORDENAR 3 NÚMEROS")
+    lista= []
+
+    lista.append( int(input("Introduzca el primer número:")) )
+    lista.append( int(input("Introduzca el primer número:")) )
+    lista.append( int(input("Introduzca el primer número:")) )
+
+    lista.sort()
+
+    print(lista)
+
+if__name__== "__main__":
+    main()
+
